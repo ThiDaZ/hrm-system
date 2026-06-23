281b5b8af3f2f7607ef6ccd1e405ffe637103639
ThiDaZ <wthidas95@gmail.com>
2026-06-23 10:50:29 +0530

    added models

---
+++ backend/app/models/models.py
@@ -0,0 +1,111 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Text, DateTime, Date, Numeric
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from app.db.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    role = Column(String, default="HR") # e.g., ADMIN, HR
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+class Department(Base):
+    __tablename__ = "departments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+    description = Column(Text, nullable=True)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    # relationship to positions
+    positions = relationship("Position", back_populates="department")
+
+class Position(Base):
+    __tablename__ = "positions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    department_id = Column(Integer, ForeignKey("departments.id"), nullable=False)
+    title = Column(String, nullable=False)
+    description = Column(Text, nullable=True)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    # relationship back to department
+    department = relationship("Department", back_populates="positions")
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+
+    id = Column(Integer, primary_key=True, index=True)
+    employee_code = Column(String, unique=True, index=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    phone = Column(String, nullable=True)
+    address = Column(Text, nullable=True)
+
+    # foreign keys linking to master data
+    department_id = Column(Integer, ForeignKey("departments.id"), nullable=False)
+    position_id = Column(Integer, ForeignKey("positions.id"), nullable=False)
+
+    joining_date = Column(Date, nullable=False)
+    employment_type = Column(String, nullable=False)  # Full-Time, Contract
+    basic_salary = Column(Numeric(10, 2), nullable=False)
+    status = Column(String, default="ONBOARDING")  # ACTIVE, INACTIVE, ONBOARDING, TERMINATED
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    # Relationships
+    department = relationship("Department")
+    position = relationship("Position")
+    documents = relationship("EmployeeDocument", back_populates="employee", cascade="all, delete-orphan")
+    payrolls = relationship("Payroll", back_populates="employee", cascade="all, delete-orphan")
+
+
+class EmployeeDocument(Base):
+    __tablename__ = "employee_documents"
+
+    id = Column(Integer, primary_key=True, index=True)
+    employee_id = Column(Integer, ForeignKey("employees.id"), nullable=False)
+    document_type = Column(String, nullable=False)  #  NIC/ID, CV
+    original_file_name = Column(String, nullable=False)
+    stored_file_name = Column(String, nullable=False)
+    file_path = Column(String, nullable=False)
+    file_size = Column(Integer, nullable=False)  # In bytes
+    mime_type = Column(String, nullable=False)  # application/pdf
+
+    uploaded_by = Column(Integer, ForeignKey("users.id"), nullable=False)
+    uploaded_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    # relationships
+    employee = relationship("Employee", back_populates="documents")
+    uploader = relationship("User")
+
+
+class Payroll(Base):
+    __tablename__ = "payrolls"
+
+    id = Column(Integer, primary_key=True, index=True)
+    employee_id = Column(Integer, ForeignKey("employees.id"), nullable=False)
+    month = Column(Integer, nullable=False)
+    year = Column(Integer, nullable=False)
+    basic_salary = Column(Numeric(10, 2), nullable=False)
+    allowances = Column(Numeric(10, 2), default=0)
+    deductions = Column(Numeric(10, 2), default=0)
+    net_salary = Column(Numeric(10, 2), nullable=False)
+    payment_status = Column(String, default="PENDING")  # PENDING, PAID, FAILED
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    # relationships
+    employee = relationship("Employee", back_populates="payrolls")
